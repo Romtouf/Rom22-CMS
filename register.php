3a69de5f9a9f6a1324ae810ebb9bfeef59330561
Romtouf <romtouf@laposte.net>
2024-07-31 13:39:42 +0200

    Création architecture du site

---
+++ register.php
@@ -0,0 +1,82 @@
+<!-- header -->
+
+<?php require_once("includes/header_register.php"); ?>
+
+
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-8">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Créer un compte</h3></div>
+                                    <div class="card-body">
+                                        <form action="register.php" method="post" enctype="multipart/form-data">
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputFirstName" type="text" name="prenom" />
+                                                        <label for="inputFirstName">Prénom</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating">
+                                                        <input class="form-control" id="inputLastName" type="text" name="nom" />
+                                                        <label for="inputLastName">Nom</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" name="email" />
+                                                <label for="inputEmail">Adresse email</label>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPassword" type="password" name="password" />
+                                                        <label for="inputPassword">Mot de passe</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPasswordConfirm" type="password" name="password_confirm" />
+                                                        <label for="inputPasswordConfirm">Confirmez le mot de passe</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputFirstName" type="text" name="username" />
+                                                        <input type="file" id="photo">
+                                                        <label for="inputFirstName">Nom d'utilisateur</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating">
+                                                        <label for="photo">Photo de profil</label>
+                                                        <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4 mb-0">
+                                                <div class="d-grid"><input type="submit" name="inscription" value="Créer un compte" class="btn btn-primary btn-block"></input></div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="login.php">Vous avez déjà un compte ? Connectez-vous</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+           
+<!-- footer -->
+           
+<?php require_once("includes/footer.php"); ?>
+
